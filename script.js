3418cab382d03c7a0849f52aa9fd619ea6c1a884
mragimov29 <mragimov29@gmail.com>
2022-11-19 03:20:18 +0400

    change colors

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//functions
 const getCurrencyData = async (firstCurrency, secondCurrency) => {
     let response = await fetch(`https://api.exchangerate.host/latest?base=${firstCurrency}&symbols=${secondCurrency}`)
     let data = response.json();
@@ -5,18 +6,18 @@ const getCurrencyData = async (firstCurrency, secondCurrency) => {
 }
 
 const printAndGetCurrency = (firstCurrency, secondCurrency) => {
-    let exchangeRates = [0, 0];
+    let exchangeRates = [];
     getCurrencyData(firstCurrency, secondCurrency).
         then(response => {
             let p = document.querySelector('.first').querySelector('.exchange-rates');
             p.innerText = `1 ${firstCurrency} = ${response.rates[`${secondCurrency}`].toFixed(4)} ${secondCurrency}`;
-            exchangeRates[0] = response.rates[`${secondCurrency}`].toFixed(4);
+            exchangeRates.push(response.rates[`${secondCurrency}`].toFixed(4));
         })
-        getCurrencyData(secondCurrency, firstCurrency).
+    getCurrencyData(secondCurrency, firstCurrency).
         then(response => {
             let p = document.querySelector('.second').querySelector('.exchange-rates');
             p.innerText = `1 ${secondCurrency} = ${response.rates[`${firstCurrency}`].toFixed(4)} ${firstCurrency}`;
-            exchangeRates[1] = response.rates[`${firstCurrency}`].toFixed(4);
+            exchangeRates.push(response.rates[`${firstCurrency}`].toFixed(4));
         })
     return exchangeRates;
 }
@@ -41,6 +42,24 @@ const change = (index, button) => {
     }
 }
 
+const removeExtraCharacters = (str) => {
+    str = str.replace(',', '.');
+    if (str.length == 1 && str[0] == '.') str = str.replace('.', '');
+
+    str = str.replace(/[A-Za-zА-Яа-я ]+/g, '');
+    str = str.replace(/[^0-9,.]/g, ' ');
+
+    let arr = str.split('');
+    let dots = arr.filter(x => x == '.');
+    if (dots.length == 2) {
+        arr.pop();
+        str = arr.join('');
+    }
+
+    return str;
+}
+
+//variables
 const input1 = document.querySelector('.first').querySelector('input');
 const input2 = document.querySelector('.second').querySelector('input');
 const currency1 = document.querySelector('.first').querySelector('#selected');
@@ -50,6 +69,7 @@ const button2 = document.querySelector('.second').querySelectorAll('button');
 let exchangeRates = printAndGetCurrency(document.querySelector('.first').querySelector('#selected').innerText,
     document.querySelector('.second').querySelector('#selected').innerText);
 
+//events
 button1.forEach((item, index) => {
     item.addEventListener('click', (event) => {
         event.target.id = 'selected';
@@ -63,39 +83,23 @@ button1.forEach((item, index) => {
 
 button2.forEach((item, index) => {
     item.addEventListener('click', (event) => {
+        // extraRates[0] = exchangeRates[0];
+        // extraRates[1] = exchangeRates[];
         event.target.id = 'selected';
         event.target.parentElement.id = 'selected';
 
         change(index, button2);
         exchangeRates = printAndGetCurrency(document.querySelector('.first').querySelector('#selected').innerText,
-            document.querySelector('.second').querySelector('#selected').innerText);
-        })
+            document.querySelector('.second').querySelector('#selected').innerText)
     })
-    
-function removeExtraCharacters(str) {
-    str = str.replace( ',', '.' );   
-    if(str.length == 1 && str[0] == '.') str = str.replace( '.', '' )
-
-    str = str.replace(/[A-Za-zА-Яа-я ]+/g, '');
-    str = str.replace(/[^0-9,.]/g, ' ');
-
-    let arr = str.split('');
-    let dots = arr.filter(x => x == '.');
-    if(dots.length == 2) {
-        arr.pop();
-        str = arr.join('');
-    }
-    
-    return str;
-}
+});
 
 input1.addEventListener('keyup', event => {
     event.target.value = removeExtraCharacters(event.target.value);
-
-    if(event.target.value != '') {
+    if (event.target.value != '') {
         let value;
         value = event.target.value * exchangeRates[0];
-        if(value % 1 == 0) 
+        if (value % 1 == 0)
             input2.value = value;
         else
             input2.value = value.toFixed(4);
@@ -104,13 +108,14 @@ input1.addEventListener('keyup', event => {
 });
 
 input2.addEventListener('keyup', event => {
+    console.log(exchangeRates)
     event.target.value = removeExtraCharacters(event.target.value);
 
-    if(event.target.value != '') {
+    if (event.target.value != '') {
         let value = event.target.value * exchangeRates[1];
-        if(value % 1 == 0)
+        if (value % 1 == 0)
             input1.value = value;
-        else 
+        else
             input1.value = value.toFixed(4);
     }
     else input1.value = '';
