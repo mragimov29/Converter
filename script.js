b0dec4503beed2b8596b655c5b1b1845ec1015bb
mragimov29 <mragimov29@gmail.com>
2022-11-19 01:19:50 +0400

    add getCurrency & change value

--- script.js
+++ script.js
@@ -1,58 +1,65 @@
-const getCurrency = async (firstCurrency, secondCurrency) => {
+const getCurrencyData = async (firstCurrency, secondCurrency) => {
     let response = await fetch(`https://api.exchangerate.host/latest?base=${firstCurrency}&symbols=${secondCurrency}`)
     let data = response.json();
     return data;
 }
 
-const printCurrency = (firstCurrency, secondCurrency) => {
-    getCurrency(firstCurrency, secondCurrency).
-    then(response => {
-        let p = document.querySelector('.first').querySelector('.exchange-rates');
-        p.innerText = `1 ${firstCurrency} = ${response.rates[`${secondCurrency}`].toFixed(4)} ${secondCurrency}`;
-    })
-    getCurrency(secondCurrency, firstCurrency).
-    then(response => {
-        let p = document.querySelector('.second').querySelector('.exchange-rates');
-        p.innerText = `1 ${secondCurrency} = ${response.rates[`${firstCurrency}`].toFixed(4)} ${firstCurrency}`;
-    })
+const printAndGetCurrency = (firstCurrency, secondCurrency) => {
+    let exchangeRates = [0, 0];
+    getCurrencyData(firstCurrency, secondCurrency).
+        then(response => {
+            let p = document.querySelector('.first').querySelector('.exchange-rates');
+            p.innerText = `1 ${firstCurrency} = ${response.rates[`${secondCurrency}`].toFixed(4)} ${secondCurrency}`;
+            exchangeRates[0] = response.rates[`${secondCurrency}`].toFixed(4);
+        })
+        getCurrencyData(secondCurrency, firstCurrency).
+        then(response => {
+            let p = document.querySelector('.second').querySelector('.exchange-rates');
+            p.innerText = `1 ${secondCurrency} = ${response.rates[`${firstCurrency}`].toFixed(4)} ${firstCurrency}`;
+            exchangeRates[1] = response.rates[`${firstCurrency}`].toFixed(4);
+        })
+    return exchangeRates;
 }
 
 const change = (index, button) => {
-    if(index === 0) {
-        button[index+1].id = ''; button[index+1].parentElement.id = '';
-        button[index+2].id= ''; button[index+2].parentElement.id= '';
-        button[index+3].id= ''; button[index+3].parentElement.id= '';
-    } else if(index === 1) {
-        button[index+1].id = ''; button[index+1].parentElement.id = '';
-        button[index+2].id = ''; button[index+2].parentElement.id = '';
-        button[index-1].id = ''; button[index-1].parentElement.id = '';
-    } else if(index === 2) {
-        button[index-2].id = ''; button[index-2].parentElement.id = '';
-        button[index-1].id = ''; button[index-1].parentElement.id = '';
-        button[index+1].id = ''; button[index+1].parentElement.id = '';
-    } else if(index === 3) {
-        button[index-3].id = ''; button[index-3].parentElement.id = '';
-        button[index-2].id = ''; button[index-2].parentElement.id = '';
-        button[index-1].id = ''; button[index-1].parentElement.id = '';
-    } 
+    if (index === 0) {
+        button[index + 1].id = ''; button[index + 1].parentElement.id = '';
+        button[index + 2].id = ''; button[index + 2].parentElement.id = '';
+        button[index + 3].id = ''; button[index + 3].parentElement.id = '';
+    } else if (index === 1) {
+        button[index + 1].id = ''; button[index + 1].parentElement.id = '';
+        button[index + 2].id = ''; button[index + 2].parentElement.id = '';
+        button[index - 1].id = ''; button[index - 1].parentElement.id = '';
+    } else if (index === 2) {
+        button[index - 2].id = ''; button[index - 2].parentElement.id = '';
+        button[index - 1].id = ''; button[index - 1].parentElement.id = '';
+        button[index + 1].id = ''; button[index + 1].parentElement.id = '';
+    } else if (index === 3) {
+        button[index - 3].id = ''; button[index - 3].parentElement.id = '';
+        button[index - 2].id = ''; button[index - 2].parentElement.id = '';
+        button[index - 1].id = ''; button[index - 1].parentElement.id = '';
+    }
 }
 
+const input1 = document.querySelector('.first').querySelector('input');
+const input2 = document.querySelector('.second').querySelector('input');
 const currency1 = document.querySelector('.first').querySelector('#selected');
 const currency2 = document.querySelector('.second').querySelector('#selected');
 const button1 = document.querySelector('.first').querySelectorAll('button');
 const button2 = document.querySelector('.second').querySelectorAll('button');
-
-printCurrency(currency1.innerText, currency2.innerText)
+let exchangeRates = printAndGetCurrency(document.querySelector('.first').querySelector('#selected').innerText,
+    document.querySelector('.second').querySelector('#selected').innerText);
 
 button1.forEach((item, index) => {
     item.addEventListener('click', (event) => {
         event.target.id = 'selected';
         event.target.parentElement.id = 'selected';
-        
+
         change(index, button1);
-        printCurrency(document.querySelector('.first').querySelector('#selected').innerText, document.querySelector('.second').querySelector('#selected').innerText)
-    })
-})
+        exchangeRates = printAndGetCurrency(document.querySelector('.first').querySelector('#selected').innerText,
+            document.querySelector('.second').querySelector('#selected').innerText);
+    });
+});
 
 button2.forEach((item, index) => {
     item.addEventListener('click', (event) => {
@@ -60,6 +67,30 @@ button2.forEach((item, index) => {
         event.target.parentElement.id = 'selected';
 
         change(index, button2);
-        printCurrency(document.querySelector('.first').querySelector('#selected').innerText, document.querySelector('.second').querySelector('#selected').innerText)
+        exchangeRates = printAndGetCurrency(document.querySelector('.first').querySelector('#selected').innerText,
+            document.querySelector('.second').querySelector('#selected').innerText);
+        })
     })
-})
+    
+input1.addEventListener('keyup', event => {
+    if(event.target.value != '') {
+        let value;
+        value = event.target.value * exchangeRates[0];
+        if(value % 1 == 0) 
+            input2.value = value;
+        else
+            input2.value = value.toFixed(4);
+    }
+    else input2.value = '';
+});
+
+input2.addEventListener('keyup', event => {
+    if(event.target.value != '') {
+        let value = event.target.value * exchangeRates[1];
+        if(value % 1 == 0)
+            input1.value = value;
+        else 
+            input1.value = value.toFixed(4);
+    }
+    else input1.value = '';
+});
